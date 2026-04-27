e9a31b42637077f2b734d8ff2756159e2ad0713f
ChipButt <jameschipbutt@hotmail.com>
2026-04-27 18:13:55 +0100

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,18 @@
+export const TILE_SIZE = 48;
+
+export const PLAYER = {
+  drawWidth: 48,
+  drawHeight: 48,
+  stepDuration: 0.22,
+  hitbox: {
+    width: 24,
+    height: 16,
+    offsetX: -12,
+    offsetY: 8
+  }
+};
+
+export const DEBUG = {
+  showCollision: false,
+  showPortalZones: false
+};
